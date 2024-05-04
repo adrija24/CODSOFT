b8d7da1c47296c1e16bce8eafad6e9d144071fae
adrija24 <24adrijaroy@gmail.com>
2024-05-04 11:00:45 +0530

    delete function is added using js

--- Calculator/script.js
+++ Calculator/script.js
@@ -10,8 +10,12 @@ Array.from(buttons).forEach((button) => {
         string = "";
         document.querySelector("input").value = string;
       } 
+      else if (e.target.innerHTML == "DEL") {
+        string = document.querySelector("input").value.slice(0,-1);
+        console.log(string);
+        document.querySelector("input").value = string;
+      } 
     else {
-      console.log(e.target);
       string = string + e.target.innerHTML;
       document.querySelector("input").value = string;
     }
